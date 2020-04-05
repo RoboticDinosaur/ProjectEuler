daf96adbd6f51d13b80f9f3045ac5dbf53f12bda
Robert Forbes <robert@roboticdinosaur.co.uk>
2020-04-05 10:30:52 +0100

    Completed challenge (needs updating later.)

---
+++ 03-Largest_prime_factor/main.go
@@ -0,0 +1,38 @@
+/* 03 - Largest prime factor
+ * -------------------------
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func PrimeFactors(n int64) (a []int64) {
+
+	for n%2 == 0 {
+		a = append(a, 2)
+		n = n / 2
+	}
+
+	var i int64
+
+	for i = 3; i*i <= n; i += 2 {
+		for n%i == 0 {
+			a = append(a, i)
+			n = n / i
+		}
+	}
+
+	if n > 2 {
+		a = append(a, n)
+
+	}
+	return
+}
+
+func main() {
+	fmt.Println(PrimeFactors(600851475143))
+}
